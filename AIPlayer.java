f3ada5cc27efe194cde8a2883493d7100f7bd811
BigCheddar2 <48729895+BigCheddar2@users.noreply.github.com>
2019-03-26 15:01:47 -0300

    Update AIPlayer.java

--- AIPlayer.java
+++ AIPlayer.java
@@ -18,7 +18,14 @@ piece to give to the opponent. The AI must be able to handle executing this
  */
 public class AIPlayer implements Player {
     
+    
+    public static Node startNode = new Node();
+    public static Piece pieceGiven;
+    public static Piece[][] board;
+    public static ArrayList<Node> tree = new ArrayList();
+    public static ArrayList<Node> copy = new ArrayList();
     public Board game;
+    
     
     private String chosenAttributes = "0000";
     private Piece pieceToGiveOpponent;
@@ -61,8 +68,9 @@ public class AIPlayer implements Player {
             return pieceToGiveOpponent;
             
         } else {
-            //midgame heuristic - Rajiv            
-            return noQuartoGetPiece();
+            //midgame heuristic - Rajiv
+            
+            return null;
         }
     }
 
@@ -78,6 +86,7 @@ public class AIPlayer implements Player {
     public boolean move(Piece piece) {
         if (game.getRemainingPieceCount() < 7) {
             //brute force method - Jacob
+            pieceGiven = piece;
             return false;
             
         } else if (Board.getPartialQuartos(game.getBoard(), '3').isEmpty()) {
@@ -96,8 +105,8 @@ public class AIPlayer implements Player {
 
         } else {
             //midgame heuristic - Rajiv
-            almostQuartoMove(piece);
-            return true;
+            
+            return false;
         }
     }
     
@@ -642,31 +651,507 @@ public class AIPlayer implements Player {
     
     
     
+    //JACOB
     
+    public static void buildTree(Node currentNode)
+    {
+        tree.add(currentNode);
+        for (int i = 0; i < currentNode.children.size(); i++)
+        {
+            buildTree(currentNode.children.get(i));
+        }
+    }
+
+    public static void drive()
+    {
+
+        board = Board.getBoard();
+
+        //create first node with current board
+        startNode.state = board.clone();
+        startNode.parent = null;
+        startNode.turn = 1;
+        //recursively get all possible states
+        getTree(startNode);
+
+        tree.clear();
+        buildTree(startNode);
+        // System.out.println(tree.size());
+
+        eliminateLosingMoves(startNode);
+        tree.clear();
+        buildTree(startNode);
+        // System.out.println(tree.size());
+
+        //REMEBER AFTER THIS
+        eliminateRedundantWins(startNode);
+        tree.clear();
+        buildTree(startNode);
+        // System.out.println(tree.size());
+
+        //copy after eliminating redundant/losing moves
+        for (int u = 0; u < tree.size(); u++)
+        {
+            copy.add(tree.get(u));
+        }
+
+        int comp1 = 1;
+        int comp2 = 0;
+
+        //loop until the method no longer changes the tree
+        while (comp1 != comp2)
+        {
+            tree.clear();
+            buildTree(startNode);
+            comp1 = tree.size();
+            removeUnfavorable(startNode);
+            tree.clear();
+            buildTree(startNode);
+            comp2 = tree.size();
+        }
+
+        tree.clear();
+        buildTree(startNode);
+
+        makeMove();
+
+    }
+
+    //Get remaining pieces for a certain state
+    public static Piece[] remainingPieces(Piece[][] thisBoard)
+    {
+        //All pieces
+        ArrayList<Piece> allPieces = new ArrayList();
+        {
+            allPieces.add(new Piece(0, true, true, true, true));
+            allPieces.add(new Piece(1, true, true, true, false));
+            allPieces.add(new Piece(2, true, true, false, true));
+            allPieces.add(new Piece(3, true, true, false, false));
+            allPieces.add(new Piece(4, true, false, true, true));
+            allPieces.add(new Piece(5, true, false, true, false));
+            allPieces.add(new Piece(6, true, false, false, true));
+            allPieces.add(new Piece(7, true, false, false, false));
+            allPieces.add(new Piece(8, false, true, true, true));
+            allPieces.add(new Piece(9, false, true, true, false));
+            allPieces.add(new Piece(10, false, true, false, true));
+            allPieces.add(new Piece(11, false, true, false, false));
+            allPieces.add(new Piece(12, false, false, true, true));
+            allPieces.add(new Piece(13, false, false, true, false));
+            allPieces.add(new Piece(14, false, false, false, true));
+            allPieces.add(new Piece(15, false, false, false, false));
+        };
+
+        //remove any piece on board from all pieces
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (thisBoard[i][j] != null)
+                {
+                    for (int g = 0; g < allPieces.size(); g++)
+                    {
+                        if (allPieces.get(g).toString()
+                            .equals(thisBoard[i][j].toString()))
+                        {
+                            allPieces.remove(g);
+                        }
+                    }
+                }
+            }
+        }
+
+        //Create a piece[] with all pieces not removed
+        Piece[] returnPieces = new Piece[allPieces.size()];
+        for (int o = 0; o < allPieces.size(); o++)
+        {
+            returnPieces[o] = allPieces.get(o);
+        }
+
+        return returnPieces;
+    }
+
+    //Get all possible routes
+    private static void getTree(Node clone)
+    {
+        //create copy of board
+        Piece[][] currentBoard = new Piece[4][4];
+        currentBoard[0] = clone.state[0].clone();
+        currentBoard[1] = clone.state[1].clone();
+        currentBoard[2] = clone.state[2].clone();
+        currentBoard[3] = clone.state[3].clone();
+
+        //get remaining pieces
+        Piece[] remaining;
+        if (clone.turn == 1)
+        {
+            Piece[] onePiece = new Piece[]
+            {
+                pieceGiven
+            };
+            remaining = onePiece;
+        }
+        else
+        {
+            remaining = remainingPieces(currentBoard);
+        }
+        //for each remaining piece
+        for (int k = 0; k < remaining.length; k++)
+        {
+            //Check all squares on the board that are null
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (currentBoard[i][j] == null)
+                    {
+
+                        //set each null square to the piece
+                        //and call getTree
+                        Piece[][] boardClone = new Piece[4][4];
+                        boardClone[0] = currentBoard[0].clone();
+                        boardClone[1] = currentBoard[1].clone();
+                        boardClone[2] = currentBoard[2].clone();
+                        boardClone[3] = currentBoard[3].clone();
+
+                        boardClone[i][j] = remaining[k];
+                        //create node with relevant data
+                        Node node = new Node();
+                        node.state = boardClone.clone();
+
+                        node.turn = clone.turn + 1;
+                        node.parent = clone;
+                        node.piecePlaced = remaining[k];
+                        clone.children.add(node);
+                        //   nodeList.add(node);
+
+                        if (!checkForQuarto(node.state))
+                        {
+                            getTree(node);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static void eliminateLosingMoves(Node node)
+    {
+        for (int i = 0; i < node.children.size(); i++)
+        {
+            ArrayList<Piece> toRemove = new ArrayList();
+            for (int k = 0; k < node.children.get(i).children.size(); k++)
+            {
+                if (checkForQuarto(node.children.get(i).children.get(k).state))
+                {
+                    toRemove.add(node.children.get(i).children.get(k).piecePlaced);
+                }
+            }
+            //remove all pieces where opponent can win
+            for (int z = 0; z < toRemove.size(); z++)
+            {
+                for (int f = 0; f < node.children.get(i).children.size(); f++)
+                {
+                    if (toRemove.get(z).toString().equals(node.children
+                        .get(i).children.get(f).piecePlaced.toString()))
+                    {
+                        node.children.get(i).children.remove(f);
+                        f--;
+                    }
+                }
+            }
+        }
+
+        //remove children that arent quarto and have no children
+        for (int g = 0; g < node.children.size(); g++)
+        {
+            if (node.children.get(g).children.isEmpty()
+                && !checkForQuarto(node.children.get(g).state))
+            {
+                node.children.remove(g);
+                g--;
+            }
+            else
+            {
+                for (int h = 0; h < node.children.get(g).children.size(); h++)
+                {
+                    eliminateLosingMoves(node.children.get(g).children.get(h));
+                }
+            }
+        }
+    }
+
+    public static void eliminateRedundantWins(Node node)
+    {
+
+        //if you can Quarto with a move, all other moves can be pruned
+        for (int i = 0; i < node.children.size(); i++)
+        {
+            if (checkForQuarto(node.children.get(i).state))
+            {
+                ArrayList<Node> toAdd = new ArrayList();
+                for (int z = 0; z < node.children.size(); z++)
+                {
+                    if (!node.children.get(z).piecePlaced.toString().
+                        equals(node.children.get(i).piecePlaced.toString()))
+                    {
+                        toAdd.add(node.children.get(z));
+                    }
+                }
+                toAdd.add(node.children.get(i));
+                node.children = toAdd;
+            }
+            else
+            {
+                eliminateRedundantWins(node.children.get(i));
+            }
+        }
+    }
+
+    private static void removeUnfavorable(Node node)
+    {
+        //if off turn (AI player)
+        if (node.turn % 2 == 1)
+        {
+            ArrayList<Node> toAdd = new ArrayList();
+            for (int i = 0; i < node.children.size(); i++)
+            {
+                if (!(!checkForQuarto(node.children.get(i).state)
+                    && node.children.get(i).children.isEmpty()))
+                {
+                    toAdd.add(node.children.get(i));
+                    removeUnfavorable(node.children.get(i));
+                }
+            }
+            node.children = toAdd;
+        }
+        //human player
+        else
+        {
+            ArrayList<Piece> toRemove = new ArrayList();
+            for (int i = 0; i < node.children.size(); i++)
+            {
+
+                if (!checkForQuarto(node.children.get(i).state)
+                    && node.children.get(i).children.isEmpty())
+                {
+                    toRemove.add(node.children.get(i).piecePlaced);
+                }
+            }
+
+            ArrayList<Node> toAdd = new ArrayList();
+            for (int j = 0; j < node.children.size(); j++)
+            {
+                int isGood = 1;
+                for (int z = 0; z < toRemove.size(); z++)
+                {
+                    if (toRemove.get(z).toString().equals(node.children.get(j).piecePlaced.toString()))
+                    {
+                        isGood = 0;
+                    }
+                }
+                if (isGood == 1)
+                {
+                    toAdd.add(node.children.get(j));
+                    removeUnfavorable(node.children.get(j));
+                }
+            }
+            node.children = toAdd;
+        }
+    }
+
+    private static Piece makeMove()
+    {
+        //You lost so make first move
+        if (copy.size() == 1)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    if (board[i][k] == null)
+                    {
+                        Board.movePiece(pieceGiven.ID, i, k);
+                        return remainingPieces(board)[0];
+                    }
+                }
+
+            }
+        }
+        //can win
+        else
+        {
+            if (startNode.children.isEmpty())
+            {
+                int foundQuarto = 1;
+                for (int s = 0; s < copy.size(); s++)
+                {
+                    if (checkForQuarto(copy.get(s).state))
+                    {
+                        foundQuarto = 0;
+                        Node current = copy.get(s);
+                        while (current.parent.parent.parent != null)
+                        {
+                            current = current.parent;
+                        }
+                        int row = -1;
+                        int col = -1;
+
+                        for (int i = 0; i < 4; i++)
+                        {
+                            for (int k = 0; k < 4; k++)
+                            {
+                                if ((current.parent.state[i][k] != null && current.parent.parent.state[i][k] == null)
+                                    || (current.parent.state[i][k] == null && current.parent.parent.state[i][k] != null))
+                                {
+                                    row = i;
+                                    col = k;
+                                }
+                            }
+                        }
+                        System.out.println("");
+                        Board.movePiece(pieceGiven.ID, row, col);
+                        return current.piecePlaced;
+                    }
+                }
+                if (foundQuarto == 1)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        for (int k = 0; k < 4; k++)
+                        {
+                            if (board[i][k] == null)
+                            {
+                                Board.movePiece(pieceGiven.ID, i, k);
+                                return remainingPieces(board)[0];
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                int row = -1;
+                int col = -1;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        if ((startNode.state[i][k] != null && startNode.children.get(0).state[i][k] == null)
+                            || (startNode.state[i][k] == null && startNode.children.get(0).state[i][k] != null))
+                        {
+                            row = i;
+                            col = k;
+                        }
+                    }
+                }
+
+                Board.movePiece(pieceGiven.ID, row, col);
+                return startNode.children.get(0).children.get(0).piecePlaced;
+
+            }
+        }
+        return null;
+    }
+
+    //store relevant data
+    public static class Node
+    {
+        public Piece[][] state;
+        public int value;
+        public Node parent;
+        public int turn;
+        public ArrayList<Node> children = new ArrayList();
+        public Piece piecePlaced;
+    }
+
+    public static boolean checkForQuarto(Piece[][] board)
+    {
+
+        ArrayList<Piece[]> toCheck = new ArrayList();
+//cols
+        for (int x = 0; x < 4; x++)
+        {
+            int isGood = 0;
+            Piece[] cols = new Piece[4];
+            for (int y = 0; y < 4; y++)
+            {
+                if (board[y][x] != null)
+                {
+                    cols[y] = board[y][x];
+                }
+                else
+                {
+                    isGood = 1;
+                }
+            }
+            if (isGood == 0)
+            {
+                toCheck.add(cols);
+            }
+        }
+        //rows
+        for (int x = 0; x < 4; x++)
+        {
+            int isGood = 0;
+            Piece[] rows = new Piece[4];
+            for (int y = 0; y < 4; y++)
+            {
+                if (board[x][y] != null)
+                {
+                    rows[y] = board[x][y];
+                }
+                else
+                {
+                    isGood = 1;
+                }
+            }
+            if (isGood == 0)
+            {
+                toCheck.add(rows);
+            }
+        }
+        //diag
+        if (board[0][0] != null
+            && board[1][1] != null
+            && board[2][2] != null
+            && board[3][3] != null)
+        {
+            Piece[] diag = new Piece[]
+            {
+                board[0][0], board[1][1], board[2][2], board[3][3]
+            };
+            toCheck.add(diag);
+        }
+        if (board[0][3] != null
+            && board[1][2] != null
+            && board[2][1] != null
+            && board[3][0] != null)
+        {
+            Piece[] diag = new Piece[]
+            {
+                board[0][3], board[1][2], board[2][1], board[3][0]
+            };
+            toCheck.add(diag);
+        }
+        for (int s = 0; s < toCheck.size(); s++)
+        {
+            int check = 0;
+            for (int d = 0; d < 4; d++)
+            {
+                check += Integer.parseInt(toCheck.get(s)[d].toString());
+            }
+            for (int h = 0; h < 4; h++)
+            {
+                if (Integer.toString(check).substring(h, h + 1).equals("8")
+                    || Integer.toString(check).substring(h, h + 1).equals("4"))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+  } 
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // JACOB //
-    
-    
-}
+
